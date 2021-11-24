d39840db296840268b08360bb456270ac5f04f2f
Garridot <garridot210@gmail.com>
2021-11-24 16:38:24 -0300

    update database

--- app/database/admin.py
+++ app/database/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class PlayersAdmin(admin.ModelAdmin):
+    list_display  = ('id','name','age','nationality','height')    
+    search_fields = ('name',)
+
+class MatchesAdmin(admin.ModelAdmin):
+    list_display  = ('id','player','season','date','competition','home_team','result','away_team','goals')
+    list_filter   = ('player','season')
+    search_fields = ('player','season')
+
+class SeasonsAdmin(admin.ModelAdmin):
+    list_display  = ('id','season')
+
+admin.site.register(Players,PlayersAdmin)
+admin.site.register(Seasons,SeasonsAdmin)
+admin.site.register(Matches,MatchesAdmin)
--- app/database/models.py
+++ app/database/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+class Players(models.Model):
+    name           =  models.CharField(max_length=200)
+    age            =  models.CharField(max_length=100)
+    date_of_birth  =  models.DateField()
+    nationality    =  models.CharField(max_length=100)
+    club           =  models.CharField(max_length=100)
+    height         =  models.CharField(max_length=100)
+    player_id      =  models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+    class Meta():
+        verbose_name        = 'Player'
+        verbose_name_plural = 'Players'    
+
+class Seasons(models.Model):
+    season         =  models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.season  
+
+    class Meta():
+        verbose_name        = 'Season'
+        verbose_name_plural = 'Seasons'      
+
+class Matches(models.Model):
+    
+    player         =  models.ForeignKey(Players,on_delete=models.CASCADE)
+    season         =  models.ForeignKey(Seasons,on_delete=models.CASCADE)
+    date           =  models.DateField()
+    competition    =  models.CharField(max_length=100)
+    home_team      =  models.CharField(max_length=100)
+    result         =  models.CharField(max_length=100)
+    away_team      =  models.CharField(max_length=100)     
+    goals          =  models.CharField(max_length=2)
+
+    class Meta():
+        verbose_name        = 'Match'
+        verbose_name_plural = 'Matches'
