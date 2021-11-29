c5286f4098c7c6986d1bf7e3574602cb6f91f493
Garridot <garridot210@gmail.com>
2021-11-29 16:24:29 -0300

    update accounts/admin

--- app/accounts/admin.py
+++ app/accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display  = ('id','email','is_active','is_staff',)    
+    search_fields = ('email',)
+    list_filter   = ('is_active','is_staff')
+
+admin.site.register(User,UserAdmin)    
