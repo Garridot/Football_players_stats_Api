9b1038872da26207502dbfa5dce14e38aa73bb3f
Garridot <garridot210@gmail.com>
2021-12-08 17:27:45 -0300

    update html

--- app/api/views.py
+++ app/api/views.py
@@ -19,7 +19,7 @@ from django.views.generic import TemplateView
 
 
 class MainView(TemplateView):
-    template_name = "main.html"
+    template_name = "main_page.html"
 
 
 class BaseViewSet(ModelViewSet):    
